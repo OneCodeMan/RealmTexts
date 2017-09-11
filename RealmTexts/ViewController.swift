b99134111eadad246d0c3b90783d5c28b407537c
Dave Gumba <dave_gumba@Yahoo.com>
2017-09-11 17:02:00 -0400

    R out of CRUD done

--- RealmTexts/ViewController.swift
+++ RealmTexts/ViewController.swift
@@ -9,49 +9,39 @@ class Entry: Object {
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    var realmEntries: Results<Entry>!
-    let realm = try? Realm()
-    
+    var realmEntries: Results<Entry>?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let realm = try? Realm()
+        realmEntries = realm?.objects(Entry.self)
+        
         tableView.delegate = self
         tableView.dataSource = self
-        
-        let entries = [
-            "First Post": "My First Post",
-            "Second Post": "Second blog post",
-            "naRCOs": "Season 3 is awesome"
-        ]
-        
-        /*
-        for (title, body) in entries {
-            let entry = Entry()
-            entry.title = title
-            entry.content = body
-            
-            try? realm?.write {
-                realm?.add(entry)
-            }
-        }
-        */
-    
-        
-        realmEntries = realm?.objects(Entry.self)
-        print(realmEntries)
-        print(realmEntries.count)
+
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return realmEntries.count
+        
+        if let count = realmEntries?.count {
+            return count
+        }
+        
+        return 0
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath)
-        //let textInfo = realmEntries(forSection: indexPath.section)[indexPath.row]
+        let entry = realmEntries?[indexPath.row]
         
-        cell.textLabel?.text = "Brotha"
+        cell.textLabel?.text = entry?.title
         
         return cell
     }
