64030efdf70b5c56801dfe9d6b6ae3bc7e9989af
Dave Gumba <dave_gumba@Yahoo.com>
2017-09-11 08:41:10 -0400

    created textcell

---
+++ RealmTexts/TextCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextCell.swift
+//  RealmTexts
+//
+//  Created by Dave Gumba on 2017-09-11.
+//  Copyright © 2017 Dave's Organization. All rights reserved.
+//
+
+import UIKit
+
+class TextCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- RealmTexts/ViewController.swift
+++ RealmTexts/ViewController.swift
@@ -6,10 +6,17 @@ class Entry: Object {
     dynamic var content = ""
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var realmEntries: Results<Entry>!
+    let realm = try? Realm()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         let entries = [
             "First Post": "My First Post",
@@ -17,8 +24,7 @@ class ViewController: UIViewController {
             "naRCOs": "Season 3 is awesome"
         ]
         
-        let realm = try? Realm()
-        
+        /*
         for (title, body) in entries {
             let entry = Entry()
             entry.title = title
@@ -28,10 +34,26 @@ class ViewController: UIViewController {
                 realm?.add(entry)
             }
         }
+        */
+    
         
-        let realmEntries = realm?.objects(Entry.self)
+        realmEntries = realm?.objects(Entry.self)
         print(realmEntries)
+        print(realmEntries.count)
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return realmEntries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath)
+        //let textInfo = realmEntries(forSection: indexPath.section)[indexPath.row]
+        
+        cell.textLabel?.text = "Brotha"
+        
+        return cell
     }
 
 }
